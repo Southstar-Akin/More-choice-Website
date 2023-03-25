b3f173bf5bf0652abd34a4244a0136ab0f32920a
Southstar <Akinbileoluwaseyi2001@gmail.com>
2023-03-25 19:56:17 +0100

    Carousel added
    Carousel added, Nav bar fixed. On to individual product pages

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-let nav = document.getElementById("navbar")
-let menu = document.getElementById("menu")
+const nav = document.getElementById("navbar")
+const menu = document.getElementById("menu")
+const banner = document.querySelector("div#taker")
+const ban = document.querySelector("div#take")
+let images = ["url(../Image/model1.jpg)", "url(../Image/model2.jpg)", "url('https://images.prestigeonline.com/wp-content/uploads/sites/8/2022/01/14190548/towfiqu-barbhuiya-0BEqLfQwVzM-unsplash-1350x900.jpg?tr=w-1920')"]
 
 nav.addEventListener( "click", navopen)
 
@@ -10,3 +13,38 @@ function navopen(){
         menu.style.display = "none"
     }
 }
+let nu = 0;
+function carousel(){
+    if (nu > images.length - 1 ){
+        nu = 0
+                banner.style.backgroundImage = images[nu];
+        banner.animate([
+            { opacity: 0, },
+            {opacity: 0.5},
+            {opacity: 0.8},
+            {opacity: 1},
+            {opacity: 0.5},
+            { opacity: 0, }
+          ], {
+            duration: 5000, 
+            fill: 'forwards' 
+          });
+        nu++
+    }else {
+        banner.style.backgroundImage = images[nu];
+        banner.animate([
+            { opacity: 0},
+            {opacity: 0.5},
+            {opacity: 0.8},
+            {opacity: 1},
+            {opacity: 0.5},
+            { opacity: 0,}
+          ], {
+            duration: 5000, 
+            fill: 'forwards' 
+          });
+        banner.style.color = "white"
+        nu++
+    }
+}
+setInterval( carousel, 5000);
