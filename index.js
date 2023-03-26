53fffacd6858db9cb43614fb1126bd14c9c786c3
Southstar <Akinbileoluwaseyi2001@gmail.com>
2023-03-26 21:02:22 +0100

    Additional image added
    Category description removed

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const nav = document.getElementById("navbar")
 const menu = document.getElementById("menu")
 const banner = document.querySelector("div#taker")
 const ban = document.querySelector("div#take")
-let images = ["url(../Image/model1.jpg)", "url(../Image/model2.jpg)", "url('https://images.prestigeonline.com/wp-content/uploads/sites/8/2022/01/14190548/towfiqu-barbhuiya-0BEqLfQwVzM-unsplash-1350x900.jpg?tr=w-1920')"]
+const sWidth = window.innerWidth
+let images = ["url(../Image/model3.jpg)", "url(../Image/model1.jpg)", "url('https://images.prestigeonline.com/wp-content/uploads/sites/8/2022/01/14190548/towfiqu-barbhuiya-0BEqLfQwVzM-unsplash-1350x900.jpg?tr=w-1920')"]
 
 nav.addEventListener( "click", navopen)
 
@@ -13,14 +14,20 @@ function navopen(){
         menu.style.display = "grid"
     }
 }
+
+if (sWidth < 760){
+  images.push("url(../Image/model2.jpg)")
+}
+
 let nu = 0;
 function carousel(){
-    if (nu > images.length - 1 ){
+    if (nu > images.length - 1){
         nu = 0
                 banner.style.backgroundImage = images[nu];
         banner.animate([
-            { opacity: 0, },
-            {opacity: 0.5},
+          { opacity: 0, },
+          { opacity: 0.3},
+          {opacity: 0.5},
             {opacity: 0.8},
             {opacity: 1},
             {opacity: 0.5},
@@ -34,6 +41,7 @@ function carousel(){
         banner.style.backgroundImage = images[nu];
         banner.animate([
             { opacity: 0},
+            { opacity: 0.3},
             {opacity: 0.5},
             {opacity: 0.8},
             {opacity: 1},
